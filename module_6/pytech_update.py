b9a970c74c50cdd11e4bf46b0505bd3a4c8e45f1
Fayette332 <135760215+Fayette332@users.noreply.github.com>
2023-07-09 22:52:23 -0500

    Add files via upload

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,1 +1,28 @@
-
+#Add the required Python code to connect to the student collection
+from pymongo import MongoClient
+client = MongoClient("localhost",27017)
+db = client["mydb"]
+pytech = db["Pytech"]
+records =[
+   {
+    "student_id": "1007",
+     "first_name": "Ruth",
+     "last_name":"Jones"
+     },
+   { 
+    "student_id": "1008",
+    "first_name": "Matthew",
+    "last_name":"Smith"
+     },
+    {
+     "student_id": "1009",
+     "first_name": "John",
+     "last_name":"Brown"
+    }
+]
+#Call the fnd() method and output the documents
+for records in records.find():
+ print(records)
+ #call the update_one method
+records.update_one({"student-id":1007, "$set":{"last_name":"Anderson"}})
+print(records)
